a22992029e5950ae25f1f6defb92e95b52084b27
Yair Even Or <Yair.Even-Or@amdocs.com>
2022-04-04 19:46:40 +0300

    added typescript support

---
+++ relative-time.d.ts
@@ -0,0 +1,23 @@
+// Definitions by: Ben Grynhaus <https://github.com/bengry>
+
+interface Settings {
+    /**
+     * @default browser locale
+     * @example 'es'
+     */
+    locale?: string;
+
+    /**
+     * @default { numeric: 'auto' }
+     */
+    options?: Intl.DateTimeFormatOptions;
+}
+
+declare class RelativeTime {
+    constructor(settings?: Settings);
+    from(d1: Date, d2?: Date): string;
+}
+
+export = RelativeTime;
+
+export as namespace RelativeTime
